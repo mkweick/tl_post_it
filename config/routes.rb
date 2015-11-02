8c796a1039c0778ee6622e69425eb8c4fdaddad4
Matt Weick <mkweick@gmail.com>
2015-11-02 01:19:47 +0000

    update comments/categories resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
   
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
-  resources :categories, only: [:new, :create, :show]
+  resources :categories, only: [:show]
 end
