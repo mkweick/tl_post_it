41d1fc77fea52fd5124ee97a368802f724bff77e
Matt Weick <mkweick@gmail.com>
2015-11-02 01:15:48 +0000

    add edit/update/destroy actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,44 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :edit, :update, :destroy]
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    # Alternative to above line:
-      # @comment = Comment.new(comment_params)
-      # @comment.post = @post
     @comment.creator = User.first #Fix after authentication
     
     if @comment.save
       redirect_to post_path(@post)
     else
-      # @post.reload needed if using .build, in memory new comment gets 
-      # added to @post.comments collection even though not saved to db yet
       @post.reload
+      @comments = @post.comments.recent
       render 'posts/show'
     end
-  end 
+  end
+  
+  def edit; end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
   
   private
+  
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+  
+  def set_comment
+    @comment = @post.comments.find(params[:id])
+  end
   
   def comment_params
     params.require(:comment).permit(:body)
