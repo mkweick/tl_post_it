ee7bc8be17b6a137e531cda1f93d59df3c99d945
Matt Weick <mkweick@gmail.com>
2015-11-02 01:14:26 +0000

    remove validations and capitalize name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,5 @@
 class Category < ActiveRecord::Base
-  before_save :capitalize_name
   has_many :post_categories, dependent: :destroy
   has_many :posts, through: :post_categories
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
   
-  private
-  
-  def capitalize_name
-    self.name = self.name.split(' ').map{ |word| word.capitalize }.join(' ')
-  end
 end
