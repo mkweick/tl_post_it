8c92f12bfb62c5f5734349e26aa118d6ab8e743f
Matt Weick <mkweick@gmail.com>
2015-11-02 01:12:34 +0000

    remove new/create category actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,31 +1,7 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
-  
-  def new
-    @category = Category.new
-  end
-  
-  def create
-    @category = Category.new(category_params)
-    
-    if @category.save
-      redirect_to category_path(@category)
-    else
-      render :new
-    end
-  end
-  
+
   def show
-    @posts = @category.posts.recent
-  end
-  
-  private
-  
-  def set_category
     @category = Category.find(params[:id])
-  end
-  
-  def category_params
-    params.require(:category).permit(:name)
+    @posts = @category.posts.recent
   end
 end
