d6fd55fc72a016ab8b4b81dc95831673dcf76223
Matt Weick <mkweick@gmail.com>
2015-11-16 20:42:10 +0000

    Add unique slug value to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,14 +9,41 @@ class Post < ActiveRecord::Base
   validates :url, presence: true
   validates :description, presence: true
   
-  before_save :generate_slug!
+  before_save :generate_slug
   
   def to_param
     self.slug
   end
   
-  def generate_slug!
-    self.slug = self.title.gsub(" ", "_").downcase
+  def generate_slug
+    the_slug = to_slug(self.title)
+    post = Post.find_by slug: the_slug
+    count = 2
+    while post && post != self
+      the_slug = append_suffix(the_slug, count)
+      post = Post.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug
+  end
+  
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    str.gsub! /-+/, '-'
+    str.downcase
+  end
+  
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      str + '-' + count.to_s
+    end
+  end
+  
+  def to_unique_slug(title)
+    title.gsub("/\A[\W]+\z/", "-")
   end
   
   def self.recent
