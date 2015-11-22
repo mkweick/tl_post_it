644f5ef2484592850c2867a6fc4e8269a43e3e4a
Matt Weick <mkweick@gmail.com>
2015-11-22 03:51:46 +0000

    preload all tables with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,35 @@ user = User.create  username: "mkweick",
                     email: "mkweick@gmail.com",
                     time_zone: "Eastern Time (US & Canada)" 
 
-9.times do
-  User.create username: Faker::Internet.user_name,
+5.times do |n|
+  User.create username: (Faker::Name.first_name.downcase + "_" + Faker::Name.last_name.downcase),
               password: "hhockey18",
               password_confirmation: "hhockey18",
               email: "example-#{n+1}@railstutorial.org",
               time_zone: "Eastern Time (US & Canada)"
 end
 
-100.times do
-  Post.create user_id: rand(1...11),
+75.times do
+  Post.create user_id: rand(1..6),
               title: Faker::Lorem.words(2).map(&:capitalize).join(' '),
               url: Faker::Internet.url,
               description: Faker::Lorem.paragraph(8, false, 0)
+end
+
+75.times do
+  PostCategory.create post_id: rand(1..65),
+                      category_id: rand(1..7)
+end
+
+125.times do
+  Comment.create user_id: rand(1..6),
+                 post_id: rand(1...65),
+                 body: Faker::Lorem.paragraph(rand(1..4), false, 0)
+end
+
+150.times do
+  type = rand(0..1)
+  Vote.create user_id: rand(1..6),
+              voteable_type: (type == 1 ? "Post" : "Comment"),
+              voteable_id: (type == 1 ? rand(1..65) : rand(1..40))
 end
